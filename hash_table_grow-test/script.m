a2e33e8b304186dff9c11158ac8a27b259c8b132
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2023-01-07 11:59:00 +0000

    MATLAB script: Change to English, remove title

--- hash_table_grow-test/script.m
+++ hash_table_grow-test/script.m
@@ -2,7 +2,7 @@ clear
 close all
 clc
 
-% Obter dados do ficheiro
+% Get data from file
 table = load("first.txt");
 j = table(:,1);
 new_size = table(:,2);
@@ -10,47 +10,42 @@ memory = table(:,3);
 free_memory = table(:,4);
 collisions = table(:,5);
 
-% Ordenar arrays free_memory e collisions (com base no free_memory)
+% Sort free_memory & collisions arrays, based on free_memory
 [free_memory_sorted,sortIdx] = sort(free_memory,'ascend');
 collisions_sorted = collisions(sortIdx);
 
-% Obter rácios
+% Get ratios
 ratio_col_mem = collisions./memory;
 ratio_col_free = collisions./free_memory;
 
 % Plots
 figure(1)
 plot(memory,collisions)
-title('Número de colisões em função da memória total')
-xlabel('Memória total (bytes)')
-ylabel('Número de colisões')
+xlabel('Total memory (bytes)')
+ylabel('Collisions')
 grid on
 
 figure(2)
 plot(free_memory_sorted,collisions_sorted)
-title('Número de colisões em função da memória livre')
-xlabel('Memória livre (bytes)')
-ylabel('Número de colisões')
+xlabel('Free memory (bytes)')
+ylabel('Collisions')
 grid on
 xlim([5000 20000])
 
 figure(3)
 plot(j,ratio_col_mem)
-title('Rácio colisões/memória total em função do incremento')
-xlabel('Incremento, j')
-ylabel('Rácio colisões/memória total')
+xlabel('Increment, j')
+ylabel('Collisions/Total memory ratio')
 grid on
 
 figure(4)
 plot(j,ratio_col_free)
-title('Rácio colisões/memória livre em função do incremento')
-xlabel('Incremento, j')
-ylabel('Rácio colisões/memória livre')
+xlabel('Increment, j')
+ylabel('Collisions/Free memory ratio')
 grid on
 
 figure(5)
 plot(memory,free_memory)
-title('Memória livre em função da memória total')
-xlabel('Memória total (bytes)')
-ylabel('Número livre (bytes)')
+xlabel('Total memory (bytes)')
+ylabel('Free memory (bytes)')
 grid on
